3af1e20a3db505003f9955f178ceaa511d2c0f95
David Evans <evansdav.a@gmail.com>
2019-09-12 14:29:10 -0700

    Added random button and functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
+from random import choice
 
 app = Flask(__name__)
 
@@ -10,17 +11,19 @@ def index():
     """Return homepage."""
     # Set parameters
     apikey = '3KIVL54QFKNV'
-    lmt = 9
+    lmt = 12
     search_term = request.args.get("search")
+    c_filter = "high"
 
     # Make dict from parameters
     params = {
         'q': search_term,
         'key': apikey,
-        'limit': lmt
+        'limit': lmt,
+        'content_filter': c_filter
         }
 
-    # Call API and load first ten results
+    # Call API and load results for search term
     r = requests.get("https://api.tenor.com/v1/search", params=params)
     if r.status_code == 200:  # If the request was successful
         first_gifs = json.loads(r.content)["results"]
@@ -35,15 +38,54 @@ def index():
 def trending():
     """Return trending gifs"""
     apikey = '3KIVL54QFKNV'
-    lmt = 9
+    lmt = 12
+    c_filter = "high"
 
     # Make dict of parameters
     params = {
         'key': apikey,
-        'limit': lmt
+        'limit': lmt,
+        'contentfilter': c_filter
     }
 
+    # Call API and load trending results
     r = requests.get("https://api.tenor.com/v1/trending", params=params)
+    if r.status_code == 200:  # If the request was successful
+        first_gifs = json.loads(r.content)["results"]
+    else:
+        first_gifs = None
+
+    return render_template("index.html", first_gifs=first_gifs)
+
+
+@app.route('/random')
+def random():
+    """Return random gifs for a trending search term"""
+    apikey = '3KIVL54QFKNV'
+    lmt = 12
+    c_filter = "high"
+
+    # Make dict of parameters
+    params = {
+        'key': apikey,
+        'limit': lmt,
+        'contentfilter': c_filter
+    }
+
+    # Call API and request a list of random search terms
+    t = requests.get("https://api.tenor.com/v1/trending_terms", params=params)
+    if t.status_code == 200:  # If the request was successful
+        term_list = json.loads(t.content)["results"]
+    else:
+        term_list = None
+
+    search = choice(term_list)
+
+    # Make add random query term to params
+    params['q'] = search
+
+    # Call API and load random results
+    r = requests.get("https://api.tenor.com/v1/random", params=params)
     if r.status_code == 200:  # If the request was successful
         first_gifs = json.loads(r.content)["results"]
     else:
